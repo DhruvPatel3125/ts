791465af77257268d9700c9e24d9bd62d8e38a3b
dhruv patel <dhruvjpatel5@gmail.com>
2025-07-12 17:01:12 +0530

    we learn basic typescript

--- index.ts
+++ index.ts
@@ -1,5 +1,142 @@
-function sum(a: number, b: number): number {
-  return a + b;
-}
+// // function sum(a: number, b: number): number {
+// //   return a + b;
+// // }
 
-console.log(sum(5, 10)); // Output: 15
+// // console.log(sum(5, 'vinod')); // Output: 15
+
+
+// // let message : string = "dhruv";
+// // console.log(message)
+
+// let mystring;
+// mystring = "dhrvu patel"
+// console.log(mystring);
+
+// let num1:number = 39493
+// let val1:string = num1.toExponential();
+// console.log(typeof num1)
+
+// let variable: null = null;
+// variable = null;
+
+// console.log(typeof variable)
+
+// console.log(variable == null)
+
+// // interface User{
+// //     username:string,
+// //     email:string,
+// // }
+
+// const newUser = {
+//     username:"dhruv",
+//     rno:23
+// }
+
+// console.log(`${newUser.username} rno is ${newUser.rno}`);
+
+// interface product{
+//     id:number;
+//     name:string;
+//     price:number;
+//     description:string;
+// }
+
+// const item:product = {
+//     id:12,
+//     name:"dejde",
+//     price:123.32,
+//     description:"efefe"
+// }
+// console.log(`${item.id} and ${item.name} and ${item.price} and ${item.description}`)
+
+// //list
+// enum data{
+//     name="dkr",
+//     rno = 23,
+//     fname = "djede"
+// };
+// console.log(data)
+
+// // tuple
+
+// const arrtuble: [number,string,string] = [12,'djej','dede'];
+// console.log(arrtuble)
+
+// //array
+
+// const arr:[number,string,string] = [12,'djede','kmkrkfr']
+// console.log(arr)
+
+// var new_arr: any[] = [1,'jcdcf']
+// new_arr.push(30);
+// new_arr.push("cdcd");
+// new_arr.push("3434");
+// console.log(new_arr)
+
+// //arrays
+
+// let fruits:string[] = ["cdjcd",'cijdic','21','true']
+// console.log(fruits)
+
+// let v1:(string | number)[] = ['apple',2,'cdcnj',54];
+
+// // let v1: Array<string|number|boolean> = ['dwd',3,'dede',true]
+// // console.log(v1)
+
+// let myArray:number[][]= [[1,233,4345],[32,454,6,7754]]
+// console.log(myArray[0][2])
+
+// let arrj:string[] = new Array("sd",'dfede','dedw')
+// for(let i = 0;i<arrj.length;i++){
+//     console.log(arrj[i])
+// }
+
+// let fr:string[] = new Array("e",'dede','wdw');
+// function def(dis_arr:string[]){
+//     for(let i=0;i<dis_arr.length;i++){
+//         console.log(arr[i])
+//     }
+// }
+// def(fr)
+
+// let val:unknown;
+// val = 5665
+// console.log(typeof val)
+// console.log(val)
+// val = true;
+// console.log(val)
+
+// let a: unknown;
+// console.log(a);
+// let b: unknown = 10;
+// console.log(b);
+// let c: any = b;
+// console.log(c); 
+
+// function display(geeks:(string|number)){
+//     if(typeof geeks =="number"){
+//         console.log('its number');
+//     }
+//     else{
+//         console.log("its string");
+//     }
+// }
+// display(32);
+
+// type type_alias = number|string|boolean;
+
+// let variable = type_alias;
+// variable = 1;
+// console.log(variable);
+// variable="sdede";
+// console.log(variable)
+// variable=true
+// console.log(variable)
+
+// typr alias_val = number|string|boolean;
+
+// let value:any = "this is a string";
+// let len : number = (value as string).length;
+// let lengthOfString: number = (value as string).length;
+// console.log(len);
